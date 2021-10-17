2de72e60bf0425f8f7dcd472ccb741d1ce5a3d1f
csaba <tschaba01@gmail.com>
2021-10-17 20:15:03 +0200

    minden

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -7,7 +7,7 @@ class GuestMailer < ApplicationMailer
     @guest = guest
     mail(
       to: guest.name_with_email,
-      subject: "#{I18n.t(:wedding_name)}: RSVP Confirmation"
+      subject: "#{I18n.t(:wedding_name)}: RSVP Visszajelzés"
     )
   end
 
@@ -15,7 +15,7 @@ class GuestMailer < ApplicationMailer
     @guest = guest
     mail(
       to: guest.name_with_email,
-      subject: "#{I18n.t(:wedding_name)}: Your RSVP"
+      subject: "#{I18n.t(:wedding_name)}: Visszajelzésed"
     )
   end
 end
